fa35c1f38cbead85e3dc260c3d7ad8cd9f1a4483
williamchavula <rumbani@gmail.com>
2025-05-08 15:59:50 -0500

    add hotel booking e2e tests

--- tests/search-hotel.spec.ts
+++ tests/search-hotel.spec.ts
@@ -44,4 +44,39 @@ test.describe('e2e tests for hotel feature', () => {
             page.getByRole('button', { name: /Book Now/ })
         ).toBeVisible();
     });
+
+    test('should should book hotel', async ({ page }) => {
+        const duration = new Date(Date.now() + 3 * 24 * 60 * 60 * 1000);
+
+        const stay = new Date();
+        stay.setDate(stay.getDate() + 3);
+        const formatted = stay.toLocaleDateString('en-US', {
+            year: 'numeric',
+            month: '2-digit',
+            day: '2-digit',
+        });
+
+        await page.goto(`${UI_URL}`);
+
+        await page.getByPlaceholder(/where are you going?/).fill('Richardson');
+        await page.getByPlaceholder('Check out date').fill(formatted);
+        await page.getByRole('button', { name: /Search/ }).click();
+        await page.getByText('The Roosevelt').click();
+
+        await page.getByRole('button', { name: /Book Now/ }).click();
+        await expect(page.getByText('Total Cost: $1,050.00')).toBeVisible();
+
+        const cardFrame = page.frameLocator(
+            'iframe[name*="__privateStripeFrame"][title*="Secure card payment input frame"]'
+        );
+
+        await cardFrame.locator('[name="cardnumber"]').fill('4242424242424242');
+        await cardFrame.locator('[name="exp-date"]').fill('12/34');
+        await cardFrame.locator('input[name="cvc"]').fill('123');
+
+        await cardFrame.getByPlaceholder('ZIP').fill('12345');
+
+        await page.getByRole('button', { name: /Confirm Booking/ }).click();
+        await expect(page.getByText('Hotel Room Booked.')).toBeVisible();
+    });
 });
